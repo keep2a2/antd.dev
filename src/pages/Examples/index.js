2a8106cd3885cd5d16692c1395abf71a6531206a
tengfei2015 <xitengfei@aragoncs.com>
2019-02-25 20:47:10 +0800

    update

---
+++ src/pages/Examples/index.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import { Layout, List, Card, Icon } from 'antd'
+import { Link, Route, Switch } from 'react-router-dom'
+
+import SortableExample from './cases/SortableExample'
+
+const { Header, Footer, Content } = Layout;
+const { Item } = List;
+
+const contentStyle = {
+    padding: '25px',
+}
+const IconStyle = {
+    marginRight: '25px',
+}
+
+const CardsList = ({match}) => {
+    const basePath = match.url.replace(/(\/*$)/g, '');
+
+    return (
+        <div className="cards=list">
+            <Card
+                title="组件示例"
+                style={{ width: 500 }}
+            >
+                <List>
+                    <Item><Link to={`${basePath}/sortable`}>拖动排序-sortable</Link></Item>
+                    <Item><Link to={`test`}>其他</Link></Item>
+                </List>
+            </Card>
+        </div>
+    )
+}
+
+class Examples extends React.Component{
+    state = {
+        referVal: [],
+        dropVal: [],
+    }
+
+    render(){
+        const {match} = this.props;
+        const basePath = match.url.replace(/(\/*$)/g, '');
+
+        console.log(match);
+
+        return (
+            <Layout className="test-page">
+                <Header>
+                    <Link to={`${basePath}/`}><Icon type="home" style={IconStyle} /></Link>
+                    Component Examples
+                </Header>
+
+                <Content style={contentStyle}>
+                    <Switch>
+                        <Route path={`${basePath}/`} exact component={CardsList}/>
+                        <Route path={`${basePath}/sortable`} component={SortableExample}/>
+                    </Switch>
+
+                </Content>
+
+                <Footer>
+                    footer
+                </Footer>
+            </Layout>
+        )
+    }
+}
+
+
+export default Examples
--- src/router/index.js
+++ src/router/index.js
@@ -3,14 +3,16 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import Home from "@/pages/Home"
 import Test from "@/pages/Test"
+import Examples from "@/pages/Examples"
 
 export default class RouteConfig extends React.Component{
     render(){
         return (
             <BrowserRouter>
                 <Switch>
-                    <Route path="/" exact component={Test} />
-                    <Route path="/test" exact component={Home} />
+                    <Route path="/" exact component={Home} />
+                    <Route path="/test" component={Test} />
+                    <Route path="/examples" component={Examples} />
                 </Switch>
             </BrowserRouter>
         )
